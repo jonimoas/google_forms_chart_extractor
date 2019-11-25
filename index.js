e77018b372cc2bd066daca3bf576b7ab480b262d
joni moas <setpsistariasbaniou@gmail.com>
2019-11-26 00:27:54 +0200

    implemented menu, percentages and chart type

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var file = "./in.xlsx";
+const electron = require("electron");
 var Excel;
 var workbook;
 var frequencies = {};
-async function parse() {
+async function parse(filename, type, percentage, out) {
   Excel = require("exceljs");
   workbook = new Excel.Workbook();
-  await workbook.xlsx.readFile(file);
+  await workbook.xlsx.readFile(filename);
   var worksheet = workbook.getWorksheet(1);
   worksheet.eachRow(function(row, rowNumber) {
     if (rowNumber != 1) {
@@ -22,36 +22,77 @@ async function parse() {
       });
     }
   });
-  makeCharts();
+  if (percentage != 0) {
+    let percentageUnit = parseInt(percentage);
+    for (const title of Object.keys(frequencies)) {
+      let sum = 0;
+      for (const field of Object.keys(frequencies[title])) {
+        sum += frequencies[title][field];
+      }
+      let tempObj = {};
+      tempObj[title] = {};
+      for (const field of Object.keys(frequencies[title])) {
+        tempObj[title][field] =
+          (frequencies[title][field] / sum) * percentageUnit;
+      }
+      frequencies[title] = tempObj[title];
+    }
+  }
+  makeCharts(type, out);
 }
-function makeCharts() {
+function makeCharts(type, out) {
   var fs = require("fs");
   var XLSXChart = require("xlsx-chart");
   var xlsxChart = new XLSXChart();
   var opts;
+  var count = Object.keys(frequencies).length;
   for (const title of Object.keys(frequencies)) {
-    console.log(frequencies[title]);
-    console.log(Object.keys(frequencies[title]));
-    var data = {};
-    data[title] = frequencies[title];
-    opts = {
-      chart: "pie",
-      titles: [title],
-      fields: Object.keys(frequencies[title]),
-      data: data,
-      chartTitle: title
-    };
-    xlsxChart.generate(opts, function(err, data) {
-      if (err) {
-        console.error(err);
-      } else {
-        fs.writeFileSync(
-          title.substring(0, 50).replace("/", "") + ".xlsx",
-          data
-        );
-        console.log(title.substring(0, 50).replace("/", "") + ".xlsx created.");
-      }
-    });
+    if (title != "") {
+      console.log(frequencies[title]);
+      var data = {};
+      data[title] = frequencies[title];
+      opts = {
+        chart: type,
+        titles: [title],
+        fields: Object.keys(frequencies[title]),
+        data: data,
+        chartTitle: title
+      };
+      xlsxChart.generate(opts, function(err, data) {
+        if (err) {
+          console.error(err);
+        } else {
+          fs.writeFileSync(
+            out + "/" + title.substring(0, 50).replace("/", "") + ".xlsx",
+            data
+          );
+          console.log(
+            title.substring(0, 50).replace("/", "") + ".xlsx created."
+          );
+        }
+        count--;
+        if (count == 0) {
+          process.exit(0);
+        }
+      });
+    }
   }
 }
-parse();
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+readline.question("Insert Filename: ", filename => {
+  readline.question(
+    `Chart Type: (column, bar, line, area, radar, scatter, pie) `,
+    type => {
+      readline.question(`Percentage: (0 = None)`, percentage => {
+        readline.question(`Output Folder: `, out => {
+          parse(filename, type, percentage, out);
+          readline.close();
+        });
+      });
+    }
+  );
+});
